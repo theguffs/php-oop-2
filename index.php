f68ddda56b1315faa20305c57ef4d205f00aed74
theguffs <youssefabdelmalak01@gmail.com>
2024-10-10 16:11:14 +0200

    esempio in html idea card

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<!-- esempio della card-->
+    <div class="col-4 mb-4">
+        <div class="card">
+            <h5 class="card-title"> cibo per cani</h5>
+            <p class="card-text">prezzo: 15</p>
+            <p class="card-text">categoria: cane</p>
+            <p class="card-text">tipo : cibo</p>
+        </div>
+
+    </div>
+</body>
+</html>
